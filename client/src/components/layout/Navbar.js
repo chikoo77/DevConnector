e5c084c77f83d17b933866d665f30a5135e41899
Frank L <frank.lm7787@gmail.com>
2018-06-28 00:45:10 -0600

    Add redirection to homepage when logging out

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { withRouter } from "react-router-dom";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
-    this.props.logoutUser();
+    this.props.logoutUser(this.props.history);
   }
   render() {
     const { isAuthenticated, user } = this.props.auth;
@@ -91,4 +92,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { logoutUser }
-)(Navbar);
+)(withRouter(Navbar));
